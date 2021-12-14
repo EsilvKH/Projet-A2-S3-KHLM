67c02a98d4f461c2524837d9ac65950b64011236
Bluebloodfr <96018713+Bluebloodfr@users.noreply.github.com>
2021-12-14 19:18:42 +0100

    Update Plateau.cs

--- Plateau.cs
+++ Plateau.cs
@@ -21,7 +21,8 @@ namespace Projet_A2_S3
             }
         }
 
-        public static readonly string[] Lignes = File.ReadAllLines(@"C:\\Temp\Scrabble\InstancePlateau.txt"); //Empêcher de la modifier
+        public static readonly string[] Lignes = File.ReadAllLines(@"C:\\Temp\Scrabble\InstancePlateau.txt"); //on prend le fichier et à chaque retour à la ligne on enregistre comme case d'un tableau de string
+        
 
         public Plateau(Sac_Jetons sacJeton)
         {
@@ -34,12 +35,37 @@ namespace Projet_A2_S3
                 _plateauMat = new Jeton[15, 15];//Plateau vide
             }
         }
-        private Jeton[,] TextToPlateau(string[] Lignes, Sac_Jetons sacjeton)
+        public bool Test_Plateau(string mot,int ligne, int colonne, char direction)
         {
-            for(int i = 0; i < Lignes.Length; i++)
+            //Il peut être placé sur le plateau en vertical ou horizontal
+            if(!placement(mot, ligne, colonne, direction))return false;
+            //Il appartient au dictionnaire
+            if(!dico(mot))return false;
+            //Les lettres de la main utiles au mot appartiennent bien à la main 
+            if(!main(mot))return false;
+            //Tous les mots qui se croisent sont éligibles
+            if(!mots())return false;
+        }
+        public bool placement(string mot, int ligne, int colonne, char direction)
+        {
+            bool plac = true;
+            if(direction == 'v')
             {
-                List<string> charList = Lignes[i].Split(';').ToList();
-                for (int j = 0; j < charList.Count; j++)
+              for(int i = 0; i<mot.Length;i++)
+              {
+                  if(_plateauMat[ligne,colonne] != null)
+                  {
+                      //je me suis arrêté là
+                  }
+              }
+            }
+        }
+        public Jeton[,] TextToPlateau(string[] Lignes, Sac_Jetons sacjeton)//cette méthode permet de passer un texte de plateau en matrice de Jetons
+        {
+            for(int i = 0; i < Lignes.Length; i++)//on parcourt le tableau de texte
+            {
+                List<string> charList = Lignes[i].Split(';').ToList();//on crée une liste de string et on sépare les char avec split en fonction des ;
+                for (int j = 0; j < charList.Count; j++)//on parcourt 
                 {
                     char LettreTxt = Convert.ToChar(charList[j]);
                     Jeton jeton = sacjeton.ListJeton.FirstOrDefault(x => x.Lettre == LettreTxt);
@@ -61,9 +87,10 @@ namespace Projet_A2_S3
                     }
                     else
                     {
-                        affichage += "| ";
+                        affichage += "_ ";
                     }
                 }
+                Console.WriteLine();
             }
             return affichage;
         }
