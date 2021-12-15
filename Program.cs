cd9e3d1d6858d9b32f87ad6beab28effe4b83db7
EsilvKH <96005677+EsilvKH@users.noreply.github.com>
2021-12-15 20:38:32 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,16 @@ namespace Projet_A2_S3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Rentrez le nom du Joueur 1");
+            string nomJoueur1 = Convert.ToString(Console.ReadLine());
+            Joueur Joueur1 = new Joueur(nomJoueur1);
+            
+            Console.WriteLine("Rentrez le nom du Joueur 2");
+            string nomJoueur2 = Convert.ToString(Console.ReadLine());
+            Joueur Joueur2 = new Joueur(nomJoueur2);
+            //On créé deux instances de la classe joueur.
+            
+            
             Sac_Jetons sacJeton = new Sac_Jetons();
             Jeton jetonRand = sacJeton.retire_jeton(new Random());
             Dictionnaire dico = new Dictionnaire("Francais", 4);
