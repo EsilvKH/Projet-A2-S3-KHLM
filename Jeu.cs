a140436adb65818d440b11c0c815c2dc4a981d20
Bluebloodfr <96018713+Bluebloodfr@users.noreply.github.com>
2021-12-15 17:37:00 +0100

    Update Jeu.cs

--- Jeu.cs
+++ Jeu.cs
@@ -6,6 +6,26 @@ namespace Projet_A2_S3
 {
     class Jeu
     {
-        private Dictionnaire[] mondico;
+        private Dictionnaire mondico;
+        private Plateau monPlateau;
+        private Sac_Jeton monsac_jeton;
+        
+        public static string[]AttributionDictionnaire(string Langue)
+        {
+            string[] LignesDictionnaire = File.ReadAllLines(@"C:\\Temp\Scrabble\"+Langue+".txt");
+            return LignesDictionnaire;
+        }
+        public static string[]AttributionPlateau(string FichierPlateau)
+        {
+            string[] LignesPlateau = File.ReadAllLines(@"C:\\Temp\Scrabble\"+FichierPlateau+".txt");
+            return LignesPlateau;
+        }
+        public static string[]AttributionSacJeton(string FichierJeton)
+        {
+            string[] LignesSacJeton = File.ReadAllLines(@"C:\\Temp\Scrabble\"+FichierJeton+".txt");
+            return LignesSacJeton;
+        }
+        public static readonly string[] Lignes = File.ReadAllLines(@"C:\\Temp\Scrabble\Jetons.txt");
+        
     }
 }
